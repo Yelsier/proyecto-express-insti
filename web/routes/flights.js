325d34724079d2f310e612fa40fe1cffe8de2359
manuelsvfz <manuel.silva@ibadia.cat>
2023-03-21 18:40:34 +0100

    Haciendo Rutas Flight

--- web/routes/flights.js
+++ web/routes/flights.js
@@ -3,8 +3,34 @@ const router = express.Router();
 const db = require("../db")
 
 //las rutas empiezan desde /api/flights
+let offset = 0;
+let limit = 10;
 router.get('/', async (req, res) => {
-    db.query("SELECT * FROM flights LIMIT 5", (err, query) => {
+
+    let actualOffset = offset;
+    if ("page" in req.params) {
+        if (!isNaN(req.params.page)) {
+            actualOffset = req.params.page * limit;
+        }
+    }
+
+    let querySearch = "SELECT * FROM flights LIMIT $1 OFFSET $2;";
+    db.query(querySearch, [limit, actualOffset], (err, query) => {
+        res.json({ data: query.rows });
+    })
+});
+
+
+router.get('/:id', async (req, res) => {
+    let querySearch = 'SELECT * FROM flights WHERE ID = $1;';
+    db.query(querySearch, [req.params.pepe], (err, query) => {
+        res.json({ data: query.rows });
+    })
+});
+
+router.get('/:limit/:offset', async (req, res) => {
+    let querySearch = 'SELECT * FROM flights LIMIT $1 OFFSET $2;';
+    db.query(querySearch, [req.params.limit, req.params.offset], (err, query) => {
         res.json({ data: query.rows });
     })
 });
