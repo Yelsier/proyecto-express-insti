57f00596b71de3f0ce11ea707c23776e0eedb2d3
Yago <clarosyago@gmail.com>
2023-03-27 19:10:17 +0200

    body parser

--- web/routes/tickets.js
+++ web/routes/tickets.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const db = require("../db")
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
-const countTickets = async () => {
-    db.query("SELECT count(*) FROM tickets", (err, query) => {
-        return query.rows
+
+const countTickets = () => {
+    return new Promise((resolve, reject) => {
+        db.query("SELECT COUNT(*) FROM tickets", (err, query) => {
+            resolve(query.rows[0].count)
+        })
     })
 }
 
 //las rutas empiezan desde /api/tickets
 router.get('/', async (req, res) => {
-    db.query("SELECT * FROM tickets LIMIT 10", (err, query) => {
-        res.json({ hola: "a" });
+    const limite = 10
+    const pages = Math.ceil(await countTickets() / limite)
+    const page = req.query.page ? req.query.page - 1 : 0
+
+    db.query("SELECT * FROM tickets LIMIT $1 OFFSET $2", [limite, page * limite], (err, query) => {
+        res.json({ tickets: query.rows, pages });
     })
 });
 
@@ -22,12 +31,23 @@ router.post('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-    db.query("SELECT * FROM tickets LIMIT 5", (err, query) => {
-        res.json({ data: query.rows });
+    db.query("SELECT * FROM tickets INNER JOIN bookings ON tickets.book_ref = bookings.book_ref WHERE ticket_no=$1", [req.params.id], (err, query) => {
+        res.json(query.rows ? query.rows[0] : {});
     })
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', jsonParser, async (req, res) => {
+    /**
+     * req type
+     * {
+     *  passenger_id
+     *  passenger_name
+     *  contact_phone
+     *  contact_email
+     *  total_amount
+     * }
+     */
+    console.log(req.body);
     db.query("SELECT * FROM tickets LIMIT 5", (err, query) => {
         res.json({ data: query.rows });
     })
